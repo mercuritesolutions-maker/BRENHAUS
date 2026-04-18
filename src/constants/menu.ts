11b2dc07f7d3b713187baaaa827a95f805060896
kristaezekiel28 <le.advena08@gmail.com>
2026-04-18 13:02:29 +0800

    update on menu

--- src/constants/menu.ts
+++ src/constants/menu.ts
@@ -1,5 +1,5 @@
 export const MENU_ITEMS = [
-  { id: 1, name: 'Handcrafted Latte', price: 145, category: 'Coffee', image: 'https://images.unsplash.com/photo-1536816579748-42bd78240d7e?auto=format&fit=crop&q=80&w=600' },
+  { id: 1, name: 'Handcrafted Latte', price: 145, category: 'Coffee', image: 'https://i.pinimg.com/1200x/51/f4/cb/51f4cbb8177ef0515912eae63b46edb5.jpg' },
   { id: 2, name: 'Signature Cold Brew', price: 160, category: 'Coffee', image: 'https://images.unsplash.com/photo-1461023058943-07fcbe16d735?auto=format&fit=crop&q=80&w=600' },
   { id: 3, name: 'Espresso Tonic', price: 155, category: 'Specialty', image: 'https://images.unsplash.com/photo-1510591509098-f4fdc6d0ff04?auto=format&fit=crop&q=80&w=600' },
   { id: 4, name: 'Matcha Cream Latte', price: 170, category: 'Tea', image: 'https://images.unsplash.com/photo-1515823064-d6e0c04616a7?auto=format&fit=crop&q=80&w=600' },
